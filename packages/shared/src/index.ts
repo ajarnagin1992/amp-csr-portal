2537423401ccf14f6f2b59be609282abb9efaaf1
Andrew Jarnagin <ajarnagin1992@gmail.com>
2026-09-17 14:56:41 -0400

    added schema to index

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -1,5 +1,6 @@
 export const APP_NAME = "AMP CSR Portal";
 export * from "./schemas/mobile-user.schema.js";
+export * from "./schemas/user-detail.schema.js";
 export * from "./schemas/update-user.schema.js";
 export * from "./schemas/create-subscription.schema.js";
 export * from "./schemas/update-subscription.schema.js";
